97ea7154daaeb2dd8a77e5469d9c1e68739a013e
foswald <falk.oswald@gmail.com>
2015-03-02 15:14:20 +0100

    Adding second AndTest

--- src/tests/bpskel/bpg/AndTest.java
+++ src/tests/bpskel/bpg/AndTest.java
@@ -3,8 +3,12 @@ package tests.bpskel.bpg;
 import org.junit.Test;
 
 import tests.bpskel.shared.TestProcessFactory;
+import tests.bpskel.shared.tasks.StaticPrintTask;
 import bpskel.bpg.api.BPGFactory;
 import bpskel.bpg.api.BusinessProcessGraph;
+import bpskel.bpg.api.ITask;
+import bpskel.bpg.impl.gateway.GatewayJoin;
+import bpskel.bpg.impl.gateway.GatewaySplit;
 import bpskel.engine.skeleton.api.SkeletonProcessEngine;
 import bpskel.engine.skeleton.impl.executor.skandium.SkandiumConnector;
 
@@ -14,8 +18,24 @@ public class AndTest {
 	public void test() {
 		BPGFactory.initialize(new SkeletonProcessEngine(SkandiumConnector.class));
 		
-		BusinessProcessGraph pro = TestProcessFactory.generateProcess1();
+		BusinessProcessGraph pro2 = new BusinessProcessGraph();
+		ITask t1 = new StaticPrintTask("FirstAndTask");
+		ITask t2 = new StaticPrintTask("SecondTask");	
 		
+		// Create And Gateway
+		GatewaySplit splitAnd1 = BPGFactory.createGatewayAndSplit();
+		GatewayJoin joinAnd1 = BPGFactory.createGatewayAndJoin();
+		
+		// Add connectors
+		pro2.connect(pro2.getStart(), splitAnd1);
+		pro2.connectFromSplit(splitAnd1, t1, t2);	
+		pro2.connectToJoin(t1,t2, joinAnd1);
+		pro2.connect(joinAnd1, pro2.getEnd());
+		
+		BPGFactory.getProcessEngine().execute(pro2);
+		
+
+		BusinessProcessGraph pro = TestProcessFactory.generateProcess1();
 		BPGFactory.getProcessEngine().execute(pro);
 	}
 
